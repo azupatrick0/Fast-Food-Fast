ef179728cdeb0fcc9caf5190a396cb10a26a0a3d
azupatrick0 <azupatrick0@gmail.com>
2018-09-04 18:37:06 +0100

    fix(Server) - fix travis ci and coveralls badges to pass [Fixes #160251760]

--- Server/src/test/testSpec.js
+++ Server/src/test/testSpec.js
@@ -1,9 +1,9 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-
+import app from '../index.js';
 
 chai.use(chaiHttp);
-//const should = chai.should();
+const should = chai.should();
 
 const newestOrder = {
     name: 'Azu Patrick',
@@ -23,30 +23,98 @@ const invalidOrder = {
     location: 'Lagos',
 }
 
+const emptyInputs = {
+  name: '',
+  email: '',
+  meal: '',
+  quantity: '',
+  price: '',
+  location: '',
+}
+
+const invalidEmail = {
+  email: 'azzz@zssscom',
+}
+
 describe('Fast-Food-Fast Test Suite', () => {
 
-  // ==== Place new order ==== //
+  // ==== Place a new order ==== //
+
   describe(' POST /orders', () => {
-    it('should insert new order to orders array', (done) => {
-      chai.request('localhost:3000')
+    it('should place a new order', (done) => {
+      chai.request(app)
         .post('/api/v1/orders')
         .send(newestOrder)
-        .end((err,res) => {
+        .end((err, res) => {
           if (err) throw err;
-          res.status.chai.should().equal(201);
+          res.status.should.equal(201);
+          res.body.should.be.a('object'); 
+          res.body.should.have.property('success'); 
+          res.body.should.have.property('data'); 
+          res.body.success.should.be.a('boolean'); 
+          res.body.data.should.be.a('object'); 
+          res.body.success.should.equal(true);
+          res.body.data.message.should.be.a('string');
+          res.body.data.message.should.equal('Your order has been processed, thank you.'); 
           done();
         });
     });
   });
 
   describe(' POST /orders', () => {
-    it('should not insert new order to orders array', (done) => {
-      chai.request('localhost:3000')
+    it('should not place a new order', (done) => {
+      chai.request(app)
         .post('/api/v1/orders')
         .send(invalidOrder)
-        .end((err,res) => {
+        .end((err, res) => {
           if (err) throw err;
-          res.status.chai.should().equal(404);
+          res.status.should.equal(404);
+          res.body.should.be.a('object'); 
+          res.body.should.have.property('success'); 
+          res.body.should.have.property('data'); 
+          res.body.success.should.be.a('boolean'); 
+          res.body.data.should.be.a('object'); 
+          res.body.success.should.equal(false);
+          res.body.data.message.should.be.a('string');
+          res.body.data.message.should.equal('Sorry, user not found, order not made'); 
+          done();
+        });
+    });
+  });
+
+  describe(' POST /orders', () => {
+    it('should return error on empty input fields and not place a new order', (done) => {
+      chai.request(app)
+        .post('/api/v1/orders')
+        .send(emptyInputs)
+        .end((err, res) => {
+          if (err) throw err;
+          res.status.should.equal(400);
+          res.body.should.be.a('object'); 
+          res.body.should.have.property('success'); 
+          res.body.should.have.property('error'); 
+          res.body.success.should.be.a('boolean'); 
+          res.body.error.should.be.a('string'); 
+          res.body.success.should.equal(false); 
+          done();
+        });
+    });
+  });
+
+  describe(' POST /orders', () => {
+    it('should return error on invalid email and not place a new order', (done) => {
+      chai.request(app)
+        .post('/api/v1/orders')
+        .send(invalidEmail)
+        .end((err, res) => {
+          if (err) throw err;
+          res.status.should.equal(400);
+          res.body.should.be.a('object'); 
+          res.body.should.have.property('success'); 
+          res.body.should.have.property('error');
+          res.body.success.should.be.a('boolean'); 
+          res.body.error.should.be.a('string'); 
+          res.body.success.should.equal(false); 
           done();
         });
     });
