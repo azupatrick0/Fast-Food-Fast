18b26814ff1f4517949ddc6bd61f4a4faec0d3ae
azupatrick0 <azupatrick0@gmail.com>
2018-09-05 08:17:47 +0100

    feat(Server): create home endpoint [Delivers #160264362]

--- Server/src/controllers/index.js
+++ Server/src/controllers/index.js
@@ -3,6 +3,7 @@ import newOrder from './newOrderController';
 import allOrders from './allOrdersController';
 import specificOrder from './specificOrderController';
 import updateStatus from './updateStatusController';
+import homePage from './homeController';
 
 // Export controllers
-export { newOrder, allOrders, specificOrder, updateStatus };
+export { newOrder, allOrders, specificOrder, updateStatus, homePage };
