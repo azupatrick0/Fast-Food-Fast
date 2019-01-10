c8750c03d67a432502db3ee920b5bbf6d732febd
Patrick Azu <azupatrick0@gmail.com>
2019-01-10 14:04:46 +0100

    [Feature #162896136]: rename Signup to signup

--- Client/src/components/index.js
+++ Client/src/components/index.js
@@ -3,6 +3,6 @@ import NavBar from './NavBar';
 import NotFound from './NotFound';
 import LandingPage from './landingPage';
 // eslint-disable-next-line import/no-named-as-default
-import SignupConnected, { Signup } from './Signup';
+import SignupConnected, { Signup } from './signup';
 
 export { Footer, NavBar, NotFound, LandingPage, Signup, SignupConnected};
